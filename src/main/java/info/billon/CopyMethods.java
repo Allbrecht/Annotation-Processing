e83981d5dc80640a4bee850341ba5c11d801af81
Albert <albert.zoraw@billon.info>
2016-12-09 13:13:24 +0100

    Annotation processor as gradle project

---
+++ src/main/java/info/billon/CopyMethods.java
@@ -0,0 +1,4 @@
+package info.billon;
+
+public @interface CopyMethods {
+}
---
+++ src/main/java/info/billon/MyProcessor.java
@@ -0,0 +1,79 @@
+package info.billon;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes("info.billon.CopyMethods")
+public class MyProcessor extends AbstractProcessor {
+    private Messager messager;
+/*    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> set = new HashSet<>();
+        set.add("CopyMethods");
+        return set;
+    }*/
+
+    //to avoid warnings
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("annotation elo");
+        messager.printMessage(Diagnostic.Kind.NOTE,
+                "Printing: annotation elo" );
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("the-file-name.txt", "UTF-8");
+            writer.println("new File!!!!");
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        writer.println("The first line");
+        writer.close();
+        return true;
+        /*
+        // Itearate over all @CopyMethods annotated elements
+        for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(CopyMethods.class)) {
+            // Check if a class has been annotated with @Factory
+            if (annotatedElement.getKind() != ElementKind.CLASS) {
+                error(annotatedElement, "Only classes can be annotated with @%s", CopyMethods.class.getSimpleName());
+                return true;
+            }
+            else {
+                    for (TypeElement te : annotations) {
+                        for (Element e : roundEnv.getElementsAnnotatedWith(te)) {
+
+                            messager.printMessage(Diagnostic.Kind.NOTE,
+                                    "Printing: " + e.toString());
+                        }
+                    }
+                    return true;
+                }
+            }
+        return true;*/
+
+    }
+    private void error(Element e, String msg, Object... args) {
+        messager.printMessage(
+                Diagnostic.Kind.ERROR,
+                String.format(msg, args),
+                e);
+    }
+
+}
